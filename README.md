7aa2b4bd004b24cf4b74f975ee21d1fd227316c0
Mike Tyszka <jmtyszka@users.noreply.github.com>
2021-07-20 15:30:58 -0700

    Working version ready for PyPi release

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Voxface
 
-Facial voxelation for 3D structural MRI
+Fast voxelation of the face in 3D structural MRI data combining spline downsampling with nearest-neighbor upsampling to
+deidentify the face while retaining some signal intensity to guide whole-head registration.
 
-
- 
+### Typical Performance
+Face voxelation of a typical 1 mm isotropic T1w image takes about 5 seconds on a 3.2 GHz 6-Core Intel Core i7 Mac Mini.
--- voxface/__main__.py
+++ voxface/__main__.py
@@ -30,7 +30,10 @@ SOFTWARE.
 """
 
 import os
+import sys
 import argparse
+import time
+
 import ants
 import pkg_resources
 import numpy as np
@@ -40,23 +43,53 @@ def main():
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Fast MRI face voxelator')
-    parser.add_argument('-i', '--infile', required=True, help='Structural MRI with intact face')
+    parser.add_argument('-i', '--infile', help='Structural MRI with intact face')
     parser.add_argument('-o', '--outfile', help="Defaced image filename")
+    parser.add_argument('--voxdim', default=8.0, type=float,
+                        help='Voxelation dimension in mm [8.0]')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False,
+                        help="Verbose output [False]")
+    parser.add_argument('-V', '--version', action='store_true', default=False,
+                        help='Display voxface version number and exit')
 
     # Parse command line arguments
     args = parser.parse_args()
-    in_fname = os.path.realpath(args.infile)
+
+    if args.infile:
+        in_fname = os.path.realpath(args.infile)
+    else:
+        print('* No faced image provided - exiting')
+        sys.exit(1)
 
     if args.outfile:
         out_fname = os.path.realpath(args.outfile)
     else:
         out_fname = in_fname.replace('.nii.gz', '_defaced.nii.gz')
 
-    print('Faced image   : {}'.format(in_fname))
-    print('Defaced image : {}'.format(out_fname))
+    # Read version from setup.py
+    ver = pkg_resources.get_distribution('voxface').version
+
+    if args.version:
+        print('VOXFACE {}'.format(ver))
+        sys.exit(1)
+
+    if args.verbose:
+
+        print('')
+        print('Facial Voxelator {}'.format(ver))
+        print('----------------')
+        print('Faced image     : {}'.format(in_fname))
+        print('Voxelated image : {}'.format(out_fname))
+        print('Voxelation size : {} mm'.format(args.voxdim))
+        print('')
+
+    # Start timer
+    tic = time.perf_counter()
 
     # Load faced image as an AntsImage
-    print('Loading faced image')
+    if args.verbose:
+        print('Loading faced image')
+
     faced_ai = ants.image_read(in_fname)
 
     # Load template image
@@ -64,7 +97,10 @@ def main():
         'voxface',
         os.path.join('templates', 'ConteCore2_50_T1w_2mm.nii.gz')
     )
-    print('Loading T1 template')
+
+    if args.verbose:
+            print('Loading T1 template')
+
     template_ai = ants.image_read(t1_template_fname)
 
     # Load deface mask
@@ -72,11 +108,16 @@ def main():
         'voxface',
         os.path.join('templates', 'ConteCore2_50_2mm_deface_mask.nii.gz')
     )
-    print('Loading template facemask')
+
+    if args.verbose:
+        print('Loading template facemask')
+
     facemask_ai = ants.image_read(facemask_fname)
 
     # Affine register template to faced image
-    print('Registering template to individual space')
+    if args.verbose:
+        print('Registering template to individual space')
+
     affine_tx = ants.registration(
         fixed=faced_ai,
         moving=template_ai,
@@ -84,7 +125,9 @@ def main():
     )
 
     # Apply transform to deface mask
-    print('Applying transform to facemask')
+    if args.verbose:
+        print('Applying transform to facemask')
+
     ind_facemask_ai = ants.apply_transforms(
         fixed=faced_ai,
         moving=facemask_ai,
@@ -92,40 +135,52 @@ def main():
         interpolator='nearestNeighbor'
     )
 
-    # Save transformed facemask
-    ind_facemask_ai.to_filename('facemask.nii.gz')
-
     # Voxelate input image
     # 1. Cubic downsample
     # 2. Nearest neighbor upsample
 
-    vox_sf = 8.0
-    old_dims = np.array(faced_ai.shape)
-    new_dims = (old_dims / vox_sf).astype(int)
+    # Get original image voxel dimensions and matrix size
+    vx, vy, vz = faced_ai.spacing
+    nx, ny, nz = faced_ai.shape
 
-    print('Spline downsampling')
+    # Downsampled matrix size to yield voxelation of requested dimensions
+    nx_dwn = np.round(nx * vx / args.voxdim).astype(int)
+    ny_dwn = np.round(ny * vy / args.voxdim).astype(int)
+    nz_dwn = np.round(nz * vz / args.voxdim).astype(int)
+
+    if args.verbose:
+        print('Spline downsampling')
+
     faced_spdwn_ai = ants.resample_image(
         image=faced_ai,
-        resample_params=new_dims,
+        resample_params=(nx_dwn, ny_dwn, nz_dwn),
         use_voxels=True,
         interp_type=4  # B-spline
     )
 
-    print('  Nearest neighbor upsampling')
+    if args.verbose:
+        print('Nearest neighbor upsampling')
+
     voxed_ai = ants.resample_image(
         image=faced_spdwn_ai,
-        resample_params=old_dims,
+        resample_params=(nx, ny, nz),
         use_voxels=True,
         interp_type=1  # Nearest neighbor
     )
 
-    # Save voxelated image
-    voxed_ai.to_filename('voxelated.nii.gz')
-
     # Replace original with voxelated image within deface mask
     ones_ai = faced_ai.new_image_like(np.ones(faced_ai.shape))
     voxfaced_ai = faced_ai * ind_facemask_ai + voxed_ai * (ones_ai - ind_facemask_ai)
-    voxfaced_ai.to_filename('voxfaced.nii.gz')
+
+    # Save face voxelated image
+    if args.verbose:
+        print('Saving face voxelated image to {}'.format(out_fname))
+
+    voxfaced_ai.to_filename(out_fname)
+
+    if args.verbose:
+        toc = time.perf_counter()
+        print('Completed in {:0.1f} seconds'.format(toc-tic))
 
 
 # This is the standard boilerplate that calls the main() function.
