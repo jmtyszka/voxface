3e8480826f86da0b0b19b811fa74b8c362678507
Mike Tyszka <jmt@caltech.edu>
2021-08-10 21:08:56 -0700

    v2021.8.10 Expand supported python versions to 3.6 to 3.9 inclusive. Force scikit-learn to 0.20.3 to avoid static TLS issue

--- setup.py
+++ setup.py
@@ -145,7 +145,10 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['antspyx>=0.2.4'],  # Optional
+    install_requires=[
+        'antspyx>=0.2.4',
+        'scikit-learn==0.20.3'
+    ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
